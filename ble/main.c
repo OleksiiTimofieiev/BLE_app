b9f9f8ef54cc49fa50fd3dbfdc9c4177df6ee474
contrbas42 <contrbas@gmail.com>
2018-10-02 15:41:12 +0300

    connection debug

--- ble/main.c
+++ ble/main.c
@@ -80,6 +80,43 @@ uint8_t boot_to_dfu = 0;
 
 void	connect_to_device(void);
 
+#define CONN_INTERVAL_MIN             80   //100ms
+#define CONN_INTERVAL_MAX             80   //100ms
+#define CONN_SLAVE_LATENCY            0    //no latency
+#define CONN_TIMEOUT                  100  //1000ms
+
+#define SCAN_INTERVAL                 16   //10ms
+#define SCAN_PASSIVE                  0
+#define SCAN_WINDOW                   16   //10ms
+
+// Health Thermometer service UUID defined by Bluetooth SIG
+//const uint8_t thermoService[2] = { 0x09, 0x18 };
+
+// Parse advertisements looking for advertised Health Thermometer service
+//uint8_t findServiceInAdvertisement(uint8_t *data, uint8_t len)
+//{
+//  uint8_t adFieldLength;
+//  uint8_t adFieldType;
+//  uint8_t i = 0;
+//  // Parse advertisement packet
+//  while (i < len) {
+//    adFieldLength = data[i];
+//    adFieldType = data[i + 1];
+//    // Partial ($02) or complete ($03) list of 16-bit UUIDs
+//    if (adFieldType == 0x02 || adFieldType == 0x03) {
+//      // compare UUID to Health Thermometer service UUID
+//      if (memcmp(&data[i + 2], thermoService, 2) == 0) {
+//        return 1;
+//      }
+//    }
+//    // advance to the next AD struct
+//    i = i + adFieldLength + 1;
+//  }
+//  return 0;
+//}
+
+int connection_status = 0;
+
 void main(void)
 {
   // Initialize device
@@ -92,8 +129,7 @@ void main(void)
   // Initialize stack
   gecko_init(&config);
 
-  int connection_status = 0;
-  int tmp;
+  bd_addr tmp;
 
   while (1) {
     /* Event pointer for handling events */
@@ -118,14 +154,50 @@ void main(void)
         /* Start general advertising and enable connections. */
         gecko_cmd_le_gap_start_advertising(0, le_gap_general_discoverable, le_gap_connectable_scannable);
         /* Connect to a specific MAC address every 1st second */
-        gecko_cmd_hardware_set_soft_timer(32768, 0, 0);
-        if(connection_status == 0)
+
+        if (connection_status == 0)
         	connect_to_device();
+        if (connection_status == 0)
+        	gecko_cmd_hardware_set_soft_timer(32768, 0, 0);
+
+        // Set passive scanning on 1Mb PHY
+//        gecko_cmd_le_gap_set_discovery_type(le_gap_phy_1m, SCAN_PASSIVE);
+        // Set scan interval and scan window
+//        gecko_cmd_le_gap_set_discovery_timing(le_gap_phy_1m, SCAN_INTERVAL, SCAN_WINDOW);
+        // Set the default connection parameters for subsequent connections
+//        gecko_cmd_le_gap_set_conn_parameters(CONN_INTERVAL_MIN,
+//                                             CONN_INTERVAL_MAX,
+//                                             CONN_SLAVE_LATENCY,
+//                                             CONN_TIMEOUT);
+        // Start scanning - looking for thermometer devices
+//        gecko_cmd_le_gap_start_discovery(le_gap_phy_1m, le_gap_discover_generic);
         break;
       }
+//      case gecko_evt_le_gap_scan_response_id:
+//             // Parse advertisement packets
+//             if (evt->data.evt_le_gap_scan_response.packet_type == 0)
+//             {
+//               // If a thermometer advertisement is found...
+//               if (findServiceInAdvertisement(&(evt->data.evt_le_gap_scan_response.data.data[0]),
+//                                              evt->data.evt_le_gap_scan_response.data.len) != 0)
+//               {
+//                 // then stop scanning for a while
+//                 gecko_cmd_le_gap_end_procedure();
+//                 // and connect to that device
+////                 if (activeConnectionsNum < MAX_CONNECTIONS) {
+//                   gecko_cmd_le_gap_connect(evt->data.evt_le_gap_scan_response.address,
+//                                            evt->data.evt_le_gap_scan_response.address_type,
+//                                            le_gap_phy_1m);
+////                   connState = opening;
+//               }
+//             }
+
       case gecko_evt_le_connection_opened_id: // check the establishment of the connection;
-    	  tmp = evt->data.evt_le_connection_opened.connection;
+    	  tmp = evt->data.evt_le_connection_opened.address;
     	  connection_status = 1;
+//          gecko_cmd_gatt_discover_primary_services_by_uuid(evt->data.evt_le_connection_opened.connection,
+//                                                           2,
+//                                                           (const uint8_t*)thermoService);
     	  break ;
       case gecko_evt_le_connection_closed_id:
         /* Check if need to boot to dfu mode */
